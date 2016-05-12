b91990e9257584cc384ea94df1cff21b1cf73cb8
nerik <erik.escoffier@gmail.com>
2016-05-12 12:22:08 +0200

    widgets women

--- env_exposed_us.js
+++ env_exposed_us.js
@@ -4,6 +4,7 @@ window.pageConfig = {
   layerIds: ["us_census","env","cities_labels"],
   zoom: 8,
   center: [7, 5],
+  basemap_opacity: 0,
   tooltip: {
     interactivity: "cartodb_id,name",
     fields: [
@@ -15,6 +16,35 @@ window.pageConfig = {
     ]
   },
   widgets: [
-
+    {
+      "id": "sum_women",
+      "type": "formula",
+      "title": "Women in childbearing age (total)",
+      "layer_id": "us_census",
+      "options": {
+        "column": "women_childbearing",
+        "operation": "sum"
+      }
+    },
+    {
+      "type": "histogram",
+      "title": "Women in childbearing age (ratio)",
+      "layer_id": "us_census",
+      "options": {
+          "type": "histogram",
+          "column": "women_childbearing_ratio",
+          "sync": true
+      }
+    },
+    {
+      "id": "sum_total_pop",
+      "type": "formula",
+      "title": "Total population",
+      "layer_id": "us_census",
+      "options": {
+        "column": "total_pop",
+        "operation": "sum"
+      }
+    },
   ]
 }
