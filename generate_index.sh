acefe233da5faf3274d0130bc3adf837d9efb745
nerik <erik.escoffier@gmail.com>
2016-05-18 18:04:29 +0200

    generate index

--- generate_index.sh
+++ generate_index.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-HTTP="/"
 OUTPUT="index.html"
 
-i=0
-echo "<UL>" > $OUTPUT
 for filepath in `find "." -name "*.html" -maxdepth 1| sort`; do
   path=`basename "$filepath"`
   echo "  <a href="$path">$path</a><br>" >> $OUTPUT
-
 done
-echo "</UL>" >> $OUTPUT
